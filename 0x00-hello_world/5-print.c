a4b8e7927b02651dd8e67d612e924cac17a594b4
ItumelengZulu <itumelengzulu@gmail.com>
2023-07-15 19:56:32 -0700

    printf funcion to display

--- 0x00-hello_world/5-print.c
+++ 0x00-hello_world/5-print.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main -  a C program that prints exactly with proper grammar, but the outcome is a piece of art,
- * requirements : do not  use puts function
- * return : 0  (success)
- */
+* main -  a C program that prints exactly with proper grammar, but the outcome is a piece of art,
+* requirements : do not  use puts function
+* return : 0  (success)
+*/
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
