01cddcf03a34ebfcdd416d279b97509b93d69731
ItumelengZulu <itumelengzulu@gmail.com>
2023-07-16 19:03:28 -0700

    binary state

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,30 +1,41 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-/**main - positive and negative
-* requirements - c change stae according to n
-* Return - 0 (Success)
-* */
-/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function initializes a variable 'n' with a random value
+ *              between -RAND_MAX/2 and RAND_MAX/2, and prints its sign.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	int n;
+    int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* my code */
-	if (n > 0)
-	{ 
-		printf('"is positive" );
-	}
-	else if (n == 0)
-	{
-		printf("is zero");
-	}
-	else (n < 0) 
-	{
-		printf("is negative");
-	}
-	
-	return (0);
+    /* Seed the random number generator */
+    srand(time(0));
+
+    /* Generate a random number between -RAND_MAX/2 and RAND_MAX/2 */
+    n = rand() - RAND_MAX / 2;
+
+    /* Print the generated number */
+    printf("The number: %d\n", n);
+
+    /* Check if the number is positive, zero, or negative */
+    if (n > 0)
+    {
+        printf("is positive\n");
+    }
+    else if (n == 0)
+    {
+        printf("is zero\n");
+    }
+    else
+    {
+        printf("is negative\n");
+    }
+
+    return (0);
 }
