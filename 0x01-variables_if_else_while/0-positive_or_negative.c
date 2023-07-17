0b143ca727372dbefd2132f58455ed8074b3a4f5
ItumelengZulu <itumelengzulu@gmail.com>
2023-07-16 19:08:25 -0700

    Binary

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,10 +4,8 @@
 
 /**
  * main - Entry point of the program
- *
  * Description: This function initializes a variable 'n' with a random value
  *              between -RAND_MAX/2 and RAND_MAX/2, and prints its sign.
- *
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -26,15 +24,15 @@ int main(void)
     /* Check if the number is positive, zero, or negative */
     if (n > 0)
     {
-        printf("is positive\n");
+        printf("is positive");
     }
     else if (n == 0)
     {
-        printf("is zero\n");
+        printf("is zero");
     }
     else
     {
-        printf("is negative\n");
+        printf("is negative");
     }
 
     return (0);
