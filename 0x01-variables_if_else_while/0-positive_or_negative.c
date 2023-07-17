d2e5714f0f831f7966683637332510406c6ffc4f
ItumelengZulu <itumelengzulu@gmail.com>
2023-07-16 18:54:17 -0700

    binary state

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,14 +15,14 @@ int main(void)
 	/* your code goes there */
 	if (n > 0)
 	{ 
-		printf('is positive');
+		printf('"is positive" );
 	}
 	else if (n == 0)
 	{
-		printf(' is zero');
+		printf("is zero");
 	}
 	else (n < 0) 
 	{
-		printf('is negative');
+		printf("is negative");
 	return (0);
 }
