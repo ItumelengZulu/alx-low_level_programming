e9696829896fb78663b3eeaabea67c0fe04a7080
ItumelengZulu <itumelengzulu@gmail.com>
2023-07-16 19:17:50 -0700

    Binary

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,27 +12,28 @@
  */
 int main(void)
 {
-    int n;
+	int n;
 
-    /* Seed the random number generator */
-    srand(time(0));
+	/* Seed the random number generator */
+	srand(time(0));
 
-    /* Generate a random number between -RAND_MAX/2 and RAND_MAX/2 */
-    n = rand() - RAND_MAX / 2;
+	/* Generate a random number between -RAND_MAX/2 and RAND_MAX/2 */
+	n = rand() - RAND_MAX / 2;
 
-    /* Check if the number is positive, zero, or negative */
-    if (n > 0)
-    {
-        printf("%d is positive\n", n);
-    }
-    else if (n == 0)
-    {
-        printf("%d is zero\n", n);
-    }
-    else
-    {
-        printf("%d is negative\n", n);
-    }
+	/* Check if the number is positive, zero, or negative */
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
 
-    return (0);
+	return (0);
 }
+
