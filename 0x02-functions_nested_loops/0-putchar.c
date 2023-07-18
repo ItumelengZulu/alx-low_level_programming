10415b0d87e738b07e089e4e849baf44b667987c
ItumelengZulu <itumelengzulu@gmail.com>
2023-07-18 06:04:53 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
 * main - prints putchar followed by new line
-* requirements - none
-* Return - _putchar as output
+* requirements - _putchar as output
+* Return - 0 (Success)
 */
-int main()
-{
+int main(){
 	printf("_putchar\n");
 	return (0);
 }
