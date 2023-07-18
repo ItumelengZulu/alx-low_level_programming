5251ba1104c098670f497fdbda22c65efec5caab
ItumelengZulu <itumelengzulu@gmail.com>
2023-07-18 06:42:21 -0700

    alphaber in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - function to print lowercase alphabets
+ * requirements - use for loop
+ * Return - 0 (Success)
+ */
+void print_alphabet(void);{
+	char c;
+
+	for c='a'; <= 'z'; c++ {
+		_putchar(c);
+	}	
+}
