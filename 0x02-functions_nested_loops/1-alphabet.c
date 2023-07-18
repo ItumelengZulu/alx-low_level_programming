3725d2affcb30b178d394a1fe48ca124faf657b6
ItumelengZulu <itumelengzulu@gmail.com>
2023-07-18 11:19:55 -0700

    10x alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,10 +4,15 @@
  * requirements - use for loop
  * Return - 0 (Success)
  */
-void print_alphabet(void) {
-	char c;
 
-	for (c='a'; c <= 'z'; c++) {
-		putchar(c);
-	}	
+void print_alphabet_x10(void) {
+  int i;
+
+  for (i = 0; i < 10; i++) {
+    for (char c = 'a'; c <= 'z'; c++) {
+      _putchar(c);
+    }
+    _putchar('\n');
+  }
 }
+
