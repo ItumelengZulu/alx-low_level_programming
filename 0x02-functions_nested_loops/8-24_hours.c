f74462697666ec5656c80c1190c657b772e5f649
ItumelengZulu <itumelengzulu@gmail.com>
2023-07-18 12:58:00 -0700

    Every minute

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,16 +1,24 @@
-
-Sure, here is the function jack_bauer() using Betty style guidelines:
-
-C
+#include "main.h"
 #include <stdio.h>
 
-void jack_bauer(void) {
-  int hour, minute;
+/**
+ * jack_bauer - Prints every minute of the day of Jack Bauer.
+ */
+void jack_bauer(void)
+{
+    int hour, minute;
 
-  for (hour = 0; hour < 24; hour++) {
-    for (minute = 0; minute < 60; minute++) {
-      printf("%02d:%02d\n", hour, minute);
+    for (hour = 0; hour < 24; hour++)
+    {
+        for (minute = 0; minute < 60; minute++)
+        {
+            _putchar('0' + (hour / 10));
+            _putchar('0' + (hour % 10));
+            _putchar(':');
+            _putchar('0' + (minute / 10));
+            _putchar('0' + (minute % 10));
+            _putchar('\n');
+        }
     }
-  }
 }
 
