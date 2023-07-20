33dde81fc96f97bd196e311a193fbe38554ac8da
ItumelengZulu <itumelengzulu@gmail.com>
2023-07-20 05:37:33 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,37 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Function prototype for _putchar */
+int _putchar(char c);
+
+/* Function prototype for _isupper */
+int _isupper(int c);
+
+/* Function prototype for _isdigit*/
+int _isdigit(int c);
+
+/* Function prototype for mul()*/
+int mul(int a, int b);
+
+/* Fucntion prototype for 0 to 10*/
+void print_numbers(void);
+
+/* Function prototype for most numbers*/
+void print_most_numbers(void);
+
+/* Function prototype for more numbers*/
+void more_numbers(void);
+
+/* Function prototype for line*/
+void print_line(int n);
+
+/* Fucntion prototype for diagonal*/
+void print_diagonal(int n);
+
+/* Function prototype for sqr*/
+void print_square(int size);
+
+/* Function prototype for triangle*/
+void print_triangle(int size);
+
+#endif /* MAIN_H */
