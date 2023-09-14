3ad70b8005551df8dee9d9697782c50b935aae2a
ItumelengZulu <itumelengzulu@gmail.com>
2023-09-14 23:48:37 +0200

    Debugging

--- 0x03-debugging/print_remaining_days.c
+++ 0x03-debugging/print_remaining_days.c
@@ -1,38 +1,41 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
-* print_remaining_days - takes a date and prints how many days are
-* left in the year, taking leap years into account
-* @month: month in number format
-* @day: day of month
-* @year: year
-* Return: void
-*/
+ * is_leap_year - Checks if a year is a leap year
+ * @year: Year to check
+ * Return: 1 if it's a leap year, 0 otherwise
+ */
+int is_leap_year(int year)
+{
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+        return 1;
+    else
+        return 0;
+}
 
+/**
+ * print_remaining_days - Takes a date and prints how many days are left
+ * in the year, taking leap years into account.
+ * @month: Month in number format (1-12)
+ * @day: Day of the month (1-31)
+ * @year: Year
+ */
 void print_remaining_days(int month, int day, int year)
 {
-    if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
-    {
-        if (month >= 2 && day >= 60)
-        {
-            day++;
-        }
+    int days_in_month[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int total_days = day;
 
-        printf("Day of the year: %d\n", day);
-        printf("Remaining days: %d\n", 366 - day);
-    }
-    else
+    if (is_leap_year(year))
     {
-        if (month == 2 && day == 60)
-        {
-            printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-        }
-        else
-        {
-            printf("Day of the year: %d\n", day);
-            printf("Remaining days: %d\n", 365 - day);
-        }
+        days_in_month[2] = 29; // February has 29 days in a leap year
     }
+
+    for (int i = 1; i < month; i++)
+    {
+        total_days += days_in_month[i];
+    }
+
+    printf("Day of the year: %d\n", total_days);
+    printf("Remaining days: %d\n", is_leap_year(year) ? 366 - total_days : 365 - total_days);
 }
 
