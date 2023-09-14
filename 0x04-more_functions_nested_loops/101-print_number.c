3db101f1f964f2806f0f709dc3b9e75be81d0fc8
ItumelengZulu <itumelengzulu@gmail.com>
2023-09-14 22:18:26 +0200

    fizz buzz

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,35 +1,22 @@
 #include "main.h"
 
-/* Function prototype for _putchar */
-int _putchar(char c);
-
-/* Function to print an integer */
-void print_number(int n) {
-    /* Handle negative numbers */
-    if (n < 0) {
-        _putchar('-');
-        n = -n;
+/**
+ * print_number - Prints an integer
+ * @n: The integer to be printed
+ */
+void print_number(int n)
+{
+    if (n < 0)
+    {
+        _putchar('-');  // Print the negative sign for negative numbers
+        n = -n;         // Make n positive for the recursive call
     }
 
-    /* Calculate the number of digits in n */
-    int numDigits = 1;
-    int temp = n;
-    while (temp / 10 != 0) {
-        temp /= 10;
-        numDigits++;
+    if (n / 10 != 0)
+    {
+        print_number(n / 10);  // Recursively print the higher-order digits
     }
 
-    /* Print each digit from left to right */
-    int divisor = 1;
-    for (int i = 1; i < numDigits; i++) {
-        divisor *= 10;
-    }
-
-    while (divisor != 0) {
-        int digit = n / divisor;
-        _putchar(digit + '0');
-        n = n % divisor;
-        divisor /= 10;
-    }
+    _putchar((n % 10) + '0');  // Print the last digit
 }
 
