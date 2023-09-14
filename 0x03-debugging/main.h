db58230d56742b27b4d17da12bf05a66363017f9
ItumelengZulu <itumelengzulu@gmail.com>
2023-09-14 08:31:25 +0200

    Debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,6 +3,12 @@
 
 #include <stdio.h>
 
+/**
+ * positive_or_negative - Determines if a number is positive, negative, or zero
+ * @i: The number to be checked
+ *
+ * Return: void
+ */
 void positive_or_negative(int i);
 
 #endif /* MAIN_H */
