ed05d992458dbec9aaaf94318c8ecbe6d8a36ff3
ItumelengZulu <itumelengzulu@gmail.com>
2023-09-19 01:43:14 +0200

    Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,30 +8,30 @@
  */
 void puts_half(char *str)
 {
-    int length = 0;
-    int i, start;
+	int length = 0;
+	int i, start;
 
-    while (str[length] != '\0')
-    {
-        length++;
-    }
+	while (str[length] != '\0')
+	{
+		length++;
+	}
 
-    /* Calculate the starting index for the second half */
-    if (length % 2 == 0)
-    {
-        start = length / 2;
-    }
-    else
-    {
-        start = (length - 1) / 2;
-    }
+	/* Calculate the starting index for the second half */
+	if (length % 2 == 0)
+	{
+		start = length / 2;
+	}
+	else
+	{
+		start = (length + 1) / 2;
+	}
 
-    /* Print the second half of the string */
-    for (i = start; i < length; i++)
-    {
-        putchar(str[i]);
-    }
+	/* Print the second half of the string */
+	for (i = start; i < length; i++)
+	{
+		putchar(str[i]);
+	}
 
-    putchar('\n');
+	putchar('\n');
 }
 
