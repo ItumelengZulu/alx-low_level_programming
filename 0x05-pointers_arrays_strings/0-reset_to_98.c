30d87a2dea0ba03dca548357311cc9a2acb55486
ItumelengZulu <itumelengzulu@gmail.com>
2023-09-18 22:22:23 +0200

    Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,20 +1,5 @@
-#include <stdio.h>
-
-/* Function prototype for reset_to_98 */
-void reset_to_98(int *n);
-
-int main(void)
+void reset_to_98(int *n)
 {
-    int n;
-
-    n = 402;
-    printf("n=%d\n", n);
-    reset_to_98(&n);
-    printf("n=%d\n", n);
-    return (0);
-}
-
-/* Implement the reset_to_98 function */
-void reset_to_98(int *n) {
     *n = 98;
 }
+
