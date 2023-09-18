c959b6cbe290a1200353ffc9f7fc6d47bab44897
ItumelengZulu <itumelengzulu@gmail.com>
2023-09-18 23:03:06 +0200

    Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+/**
+ * _strlen - Returns the length of a string.
+ * @s: A pointer to the string.
+ *
+ * Return: The length of the string (an integer).
+ */
+int _strlen(char *s)
+{
+    int length = 0;
+
+    while (*s != '\0')
+    {
+        length++;
+        s++;
+    }
+
+    return length;
+}
+
