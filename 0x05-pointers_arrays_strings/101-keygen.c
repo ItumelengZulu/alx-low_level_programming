ca2e8c4cf0840e95bdfadaa2c409b083bb2d2f83
ItumelengZulu <itumelengzulu@gmail.com>
2023-09-19 03:09:42 +0200

    Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -4,16 +4,21 @@
 
 #define PASSWORD_LENGTH 12
 
+/**
+ * main - Generates a random valid password for 101-crackme
+ *
+ * Return: 0 on success
+ */
 int main(void)
 {
-    char password[PASSWORD_LENGTH + 1];  // +1 for the null terminator
+    char password[PASSWORD_LENGTH + 1];  /* +1 for the null terminator */
     const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     int i;
 
-    // Seed the random number generator with the current time
+    /* Seed the random number generator with the current time */
     srand(time(NULL));
 
-    // Generate a random password
+    /* Generate a random password */
     for (i = 0; i < PASSWORD_LENGTH; i++)
     {
         int index = rand() % (sizeof(charset) - 1);
@@ -21,7 +26,7 @@ int main(void)
     }
     password[PASSWORD_LENGTH] = '\0';
 
-    // Print the generated password
+    /* Print the generated password */
     printf("%s\n", password);
 
     return (0);
