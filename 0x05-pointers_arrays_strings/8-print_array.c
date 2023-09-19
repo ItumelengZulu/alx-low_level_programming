ea50b578a97abe5b0d90b8257b571202a167f4f8
ItumelengZulu <itumelengzulu@gmail.com>
2023-09-19 02:01:14 +0200

    Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
- * print_array - Prints n elements of an array of integers, followed by a new line.
+ * print_array - Prints n elements of an array of integers.
  * @a: A pointer to the array of integers.
  * @n: The number of elements to print.
  *
- * Return: No return value (void).
+ * Description: This function prints the elements of an integer array.
+ *              If n is less than or equal to 0, it prints "Return SUCCESS".
  */
 void print_array(int *a, int n)
 {
@@ -25,23 +26,9 @@ void print_array(int *a, int n)
 
         if (i < n - 1)
         {
-            printf(",");
+            printf(","); /* Add a comma if it's not the last element */
         }
     }
 
     printf(" };\n");
 }
-
-int main(void)
-{
-    int array1[5] = { -198, 298, 402, -1024, 98 };
-    int array2[256]; // Assuming you have initialized this array with your big data.
-    int array3[1] = { 98 };
-
-    print_array(array1, 5);
-    print_array(array2, 256);
-    print_array(array3, 1);
-
-    return (0);
-}
-
