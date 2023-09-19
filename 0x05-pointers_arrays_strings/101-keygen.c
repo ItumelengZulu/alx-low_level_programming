669cb4476fe7bb552ec01f8dc331380d10a20c54
ItumelengZulu <itumelengzulu@gmail.com>
2023-09-19 03:07:29 +0200

    Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,67 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <ctype.h>
 
-/**
- * generate_password - Generates a random valid password for 101-crackme.
- *
- * Return: A random valid password as a dynamically allocated string.
- */
-char *generate_password(void)
+#define PASSWORD_LENGTH 12
+
+int main(void)
 {
-        static char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-        char *password = malloc(12); /* Password length is 11 characters, plus 1 for the null terminator */
-        int i;
-        int lowercase_count = 0;
-        int uppercase_count = 0;
-        int digit_count = 0;
+    char password[PASSWORD_LENGTH + 1];  // +1 for the null terminator
+    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    int i;
 
-        if (password == NULL)
-        {
-                perror("malloc");
-                exit(EXIT_FAILURE);
-        }
+    // Seed the random number generator with the current time
+    srand(time(NULL));
 
-        srand(time(NULL));
+    // Generate a random password
+    for (i = 0; i < PASSWORD_LENGTH; i++)
+    {
+        int index = rand() % (sizeof(charset) - 1);
+        password[i] = charset[index];
+    }
+    password[PASSWORD_LENGTH] = '\0';
 
-        for (i = 0; i < 11; i++)
-        {
-                int index = rand() % 62; /* 62 is the length of the charset */
-                char random_char = charset[index];
-                password[i] = random_char;
+    // Print the generated password
+    printf("%s\n", password);
 
-                if (islower(random_char)) {
-                    lowercase_count++;
-                } else if (isupper(random_char)) {
-                    uppercase_count++;
-                } else if (isdigit(random_char)) {
-                    digit_count++;
-                }
-        }
-
-        if (lowercase_count == 0 || uppercase_count == 0 || digit_count == 0) {
-            free(password);
-            return generate_password();
-        }
-
-        password[11] = '\0'; /* Null-terminate the password */
-
-        return password;
-}
-
-int pass_main(void)
-{
-        char *password = generate_password();
-        printf("Generated Password: %s\n", password);
-
-        if (is_valid_password(password)) {
-            printf("Password is valid.\n");
-        } else {
-            printf("Password is not valid.\n");
-        }
-
-        free(password);
-        return 0;
+    return (0);
 }
 
