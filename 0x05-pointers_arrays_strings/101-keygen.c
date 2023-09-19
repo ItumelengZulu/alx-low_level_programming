c09ca1c99632bcfd985a8476ebc3826fdd2fc18b
ItumelengZulu <itumelengzulu@gmail.com>
2023-09-19 02:59:17 +0200

    Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <ctype.h>
 
 /**
  * generate_password - Generates a random valid password for 101-crackme.
@@ -9,34 +10,59 @@
  */
 char *generate_password(void)
 {
-	static char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-	char *password = malloc(12); /* Password length is 11 characters, plus 1 for the null terminator */
-	int i;
+        static char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        char *password = malloc(12); /* Password length is 11 characters, plus 1 for the null terminator */
+        int i;
+        int lowercase_count = 0;
+        int uppercase_count = 0;
+        int digit_count = 0;
 
-	if (password == NULL)
-	{
-		perror("malloc");
-		exit(EXIT_FAILURE);
-	}
+        if (password == NULL)
+        {
+                perror("malloc");
+                exit(EXIT_FAILURE);
+        }
 
-	srand(time(NULL));
+        srand(time(NULL));
 
-	for (i = 0; i < 11; i++)
-	{
-		int index = rand() % 62; /* 62 is the length of the charset */
-		password[i] = charset[index];
-	}
+        for (i = 0; i < 11; i++)
+        {
+                int index = rand() % 62; /* 62 is the length of the charset */
+                char random_char = charset[index];
+                password[i] = random_char;
 
-	password[11] = '\0'; /* Null-terminate the password */
+                if (islower(random_char)) {
+                    lowercase_count++;
+                } else if (isupper(random_char)) {
+                    uppercase_count++;
+                } else if (isdigit(random_char)) {
+                    digit_count++;
+                }
+        }
 
-	return password;
+        // Ensure at least one lowercase letter, one uppercase letter, and one digit
+        if (lowercase_count == 0 || uppercase_count == 0 || digit_count == 0) {
+            free(password);
+            return generate_password(); // Regenerate the password if criteria not met
+        }
+
+        password[11] = '\0'; /* Null-terminate the password */
+
+        return password;
 }
 
 int main(void)
 {
-	char *password = generate_password();
-	printf("%s\n", password);
-	free(password); /* Free the dynamically allocated memory */
-	return 0;
+        char *password = generate_password();
+        printf("Generated Password: %s\n", password);
+
+        if (is_valid_password(password)) {
+            printf("Password is valid.\n");
+        } else {
+            printf("Password is not valid.\n");
+        }
+
+        free(password);
+        return 0;
 }
 
