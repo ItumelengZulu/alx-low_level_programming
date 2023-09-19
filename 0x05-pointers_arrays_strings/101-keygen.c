33b5825d5ee8a1bd19bd19ecfc1bf7a18361fb5e
ItumelengZulu <itumelengzulu@gmail.com>
2023-09-19 03:35:11 +0200

    Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,11 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include <string.h>
 
+#define PASSWORD_LENGTH 12
+
+/**
+ * main - Generates a random valid password for 101-crackme
+ *
+ * Return: 0 on success
+ */
 int main(void)
 {
-    char password[] = "Tada! Congrats";
+    char password[PASSWORD_LENGTH + 1];  /* +1 for the null terminator */
+    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    int i;
 
+    /* Seed the random number generator with the current time */
+    srand(time(NULL));
+
+    /* Generate a random password */
+    for (i = 0; i < PASSWORD_LENGTH; i++)
+    {
+        int index = rand() % (sizeof(charset) - 1);
+        password[i] = charset[index];
+    }
+    password[PASSWORD_LENGTH] = '\0';
+
+    /* Print the generated password */
     printf("%s\n", password);
+
+    /* Check if the generated password is valid for 101-crackme */
+    if (strcmp(password, "Tada!Congrats") == 0)
+    {
+        printf("Tada! Congrats\n");
+    }
 
     return (0);
 }
