bed92f8cf15cf00e0a513d786709dbdd49c553cb
ItumelengZulu <itumelengzulu@gmail.com>
2023-09-19 05:01:06 +0200

    Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,35 +7,44 @@
  *
  * Return: A pointer to the beginning of the destination buffer.
  */
-char *_strcpy(
-	char *dest, 
-	const char *src
-	)
+char *_strcpy(char *dest, const char *src)
 {
-	char *dest_start = dest; /* Store the starting address of dest */
+    char *dest_start = dest; /* Store the starting address of dest */
 
-	/* Copy each character from src to dest until the null terminator is encountered */
-	while (*src != '\0')
-	{
-		*dest = *src; /* Copy the character */
-		dest++;       /* Move dest and src pointers to the next character */
-		src++;
-	}
+    /* Copy each character from src to dest until the null terminator is encountered */
+    while (*src != '\0')
+    {
+        *dest = *src; /* Copy the character */
+        dest++;       /* Move dest and src pointers to the next character */
+        src++;
+    }
 
-	*dest = '\0'; /* Add the null terminator to the destination string */
-	return (dest_start); /* Return a pointer to the beginning of dest */
+    *dest = '\0'; /* Add the null terminator to the destination string */
+    return (dest_start); /* Return a pointer to the beginning of dest */
 }
 
+/**
+ * array_main - Demonstrates the usage of the _strcpy function.
+ *
+ * This function initializes source and destination strings and then uses the
+ * _strcpy function to copy the source string into the destination buffer.
+ * It prints the copied string and the pointer to the destination buffer.
+ */
 int array_main(void)
 {
-	char src[] = "Hello, World!";
-	char dest[20]; /* Make sure dest has enough space to hold the copied string */
+    char src[] = "Hello, World!";
+    char dest[20]; /* Make sure dest has enough space to hold the copied string */
 
-	char *result = _strcpy(dest, src);
+    char *result = _strcpy(dest, src);
 
-	printf("Copied string: %s\n", dest);
-	printf("Pointer to dest: %p\n", (void *)result);
+    printf("Copied string: %s\n", dest);
+    printf("Pointer to dest: %p\n", (void *)result);
 
-	return (0);
+    return (0);
+}
+
+int main(void)
+{
+    return (array_main());
 }
 
