9285f2a3d6f04a41a2812bd39dbb5cc0f6fadd1f
ItumelengZulu <itumelengzulu@gmail.com>
2023-09-22 04:11:47 +0200

    Pointers, arrays and strings

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,24 +1,18 @@
 /**
- * rot13 - Encodes a string using ROT13.
+ * rot13 - Encodes a string using rot13.
  * @str: The string to be encoded.
  *
  * Return: A pointer to the modified string.
  */
 char *rot13(char *str)
 {
-    char *ptr = str; /* Create a pointer to the original string for returning later */
-
-    while (*str) {
-        char c = *str;
-        
-        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
-            char base = (c >= 'a' && c <= 'z') ? 'a' : 'A';
-            *str = (((c - base) + 13) % 26) + base;
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] >= 'a' && str[i] <= 'z') {
+            str[i] = (str[i] - 'a' + 13) % 26 + 'a';
+        } else if (str[i] >= 'A' && str[i] <= 'Z') {
+            str[i] = (str[i] - 'A' + 13) % 26 + 'A';
         }
-
-        str++;
     }
-
-    return ptr; /* Return the modified string */
+    return str;
 }
 
