684e07de9a83a1b56645a4326df5b61e787a2b04
ItumelengZulu <itumelengzulu@gmail.com>
2023-09-22 04:07:24 +0200

    Pointers, arrays and strings

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,24 +1,32 @@
-char *leet(char *str) {
-  char *ptr = str; /* Create a pointer to the original string for returning later */
+/**
+ * leet - Encodes a string into 1337.
+ * @str: The string to be encoded.
+ *
+ * Return: A pointer to the modified string.
+ */
+char *leet(char *str)
+{
+    char *ptr = str; /* Create a pointer to the original string for returning later */
+    int i;
 
-  for (int i = 0; str[i] != '\0'; i++) {
-    while (*str) {
-      char c = *str;
-      if (c == 'a' || c == 'A') {
-        *str = '4';
-      } else if (c == 'e' || c == 'E') {
-        *str = '3';
-      } else if (c == 'o' || c == 'O') {
-        *str = '0';
-      } else if (c == 't' || c == 'T') {
-        *str = '7';
-      } else if (c == 'l' || c == 'L') {
-        *str = '1';
-      }
-      str++;
+    for (i = 0; str[i] != '\0'; i++) {
+        while (*str) {
+            char c = *str;
+            if (c == 'a' || c == 'A') {
+                *str = '4';
+            } else if (c == 'e' || c == 'E') {
+                *str = '3';
+            } else if (c == 'o' || c == 'O') {
+                *str = '0';
+            } else if (c == 't' || c == 'T') {
+                *str = '7';
+            } else if (c == 'l' || c == 'L') {
+                *str = '1';
+            }
+            str++;
+        }
     }
-  }
 
-  return ptr; /* Return the modified string */
+    return ptr; /* Return the modified string */
 }
 
