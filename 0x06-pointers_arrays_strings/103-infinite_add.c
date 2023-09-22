daaf09f01f6c788b1639787cb2138f5cd8f7f722
ItumelengZulu <itumelengzulu@gmail.com>
2023-09-22 04:20:09 +0200

    Pointers, arrays and strings

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,83 +1,22 @@
-#include "main.h"
-#include <stdio.h>
-
-/**
- * infinite_add - Adds two numbers represented as strings.
- * @n1: The first number as a string.
- * @n2: The second number as a string.
- * @r: The buffer to store the result.
- * @size_r: The size of the buffer.
- *
- * Return: A pointer to the result (r) or 0 if the result cannot be stored.
- */
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
-{
-    int carry = 0;
-    int i = 0;
-    int len1 = 0;
-    int len2 = 0;
-    int start = 0; /* Declare start here */
-    int end = 0;   /* Declare end here */
-
-    while (n1[len1] != '\0')
-        len1++;
-    while (n2[len2] != '\0')
-        len2++;
-
-    if (size_r <= len1 || size_r <= len2)
-        return 0; /* Result cannot be stored in the given buffer size */
-
-    r[size_r] = '\0';
-    len1--;
-    len2--;
-
-    while (len1 >= 0 || len2 >= 0) {
-        int num1 = (len1 >= 0) ? (n1[len1] - '0') : 0;
-        int num2 = (len2 >= 0) ? (n2[len2] - '0') : 0;
-        int sum = num1 + num2 + carry;
-        carry = sum / 10;
-        r[i] = (sum % 10) + '0';
-        i++;
-        len1--;
-        len2--;
-    }
-
-    if (carry) {
-        if (i >= size_r - 1)
-            return 0; /* Result cannot be stored in the given buffer size */
-        r[i] = carry + '0';
-        i++;
-    }
-
-    r[i] = '\0';
-
-    /* Reverse the result string */
-    start = 0; /* Assign values here */
-    end = i - 1; /* Assign values here */
-    while (start < end) {
-        char temp = r[start];
-        r[start] = r[end];
-        r[end] = temp;
-        start++;
-        end--;
-    }
-
-    return r;
-}
-
-int main(void)
-{
-    char num1[] = "123456789012345678901234567890";
-    char num2[] = "987654321098765432109876543210";
-    char result[100];
-
-    char *sum = infinite_add(num1, num2, result, sizeof(result));
-    if (sum) {
-        printf("Sum: %s\n", sum);
-    } else {
-        printf("Result cannot be stored in the buffer.\n");
-    }
-
+char *infinite_add(char *n1, char *n2, char *r, int size_r) {
+  // Check if the buffer is large enough to store the result.
+  if (size_r < (strlen(n1) + strlen(n2) + 1)) {
     return 0;
+  }
+
+  // Initialize the carry.
+  int carry = 0;
+
+  // Iterate over the two numbers, adding each digit and the carry.
+  for (int i = strlen(n1) - 1, j = strlen(n2) - 1, k = size_r - 1; i >= 0 || j >= 0 || carry; i--, j--, k--) {
+    int digit1 = i >= 0 ? n1[i] - '0' : 0;
+    int digit2 = j >= 0 ? n2[j] - '0' : 0;
+    int sum = digit1 + digit2 + carry;
+    carry = sum / 10;
+    r[k] = '0' + (sum % 10);
+  }
+
+  // Return a pointer to the result.
+  return r;
 }
 
