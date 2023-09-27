96602eafb17306cdd62fbe8802fae74aa8fe913c
ItumelengZulu <itumelengzulu@gmail.com>
2023-09-27 18:26:45 +0200

    Recursion

---
+++ 0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string followed by a new line.
+ * @s: The string to be printed.
+ */
+void _puts_recursion(char *s)
+{
+    if (*s == '\0') /* Check for the end of the string */
+    {
+        _putchar('\n'); /* Print a new line when the end of the string is reached */
+        return;
+    }
+
+    _putchar(*s); /* Print the current character */
+    _puts_recursion(s + 1); /* Move to the next character in the string and call the function recursively */
+}
+
---
+++ main.h
@@ -0,0 +1,35 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Function prototypes */
+void _puts_recursion(char *s);
+
+/* Function prototypes */
+int _putchar(char c);
+
+/* Function prototypes */
+void _print_rev_recursion(char *s);
+
+/* Function prototypes */
+int _strlen_recursion(char *s);
+
+/* Function prototypes */
+int factorial(int n);
+
+/* Function prototypes */
+int _pow_recursion(int x, int y);
+
+/* Function prototypes */
+int _sqrt_recursion(int n);
+
+/* Function prototypes */
+int is_prime_number(int n);
+
+/* Function prototypes */
+int is_palindrome(char *s);
+
+/* Function prototypes */
+int wildcmp(char *s1, char *s2);
+
+#endif /* MAIN_H */
+
