6f76df15d5052af8bc09777006a10f4ca6ad7d9c
ItumelengZulu <itumelengzulu@gmail.com>
2023-09-27 19:23:18 +0200

    Recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -12,17 +12,17 @@
  */
 int _pow_recursion(int x, int y)
 {
-    if (y < 0) /* Check for negative exponent */
-    {
-        return -1; /* Return -1 to indicate an error */
-    }
-    else if (y == 0) /* Base case: x^0 is 1 */
-    {
-        return 1;
-    }
-    else
-    {
-        return x * _pow_recursion(x, y - 1); /* Recursive call to calculate x^y */
-    }
+	if (y < 0) /* Check for negative exponent */
+	{
+		return (-1); /* Return -1 to indicate an error */
+	}
+	else if (y == 0) /* Base case: x^0 is 1 */
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1)); /* Recursive call to calculate x^y */
+	}
 }
 
