30a98bdaceab837c9248d2b6e68d873c9e13d414
ItumelengZulu <itumelengzulu@gmail.com>
2023-09-27 18:52:09 +0200

    Recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -12,11 +12,11 @@ int _strlen_recursion(char *s)
 {
 	if (*s == '\0') /* Base case: end of the string */
 	{
-		return 0; /* Return 0 when the end of the string is reached */
+		return (0); /* Return 0 when the end of the string is reached */
 	}
 	else
 	{
-		return 1 + _strlen_recursion(s + 1); /* Recursive call with the next character */
+		return (1 + _strlen_recursion(s + 1)); /* Recursive call with the next character */
 	}
 }
 
