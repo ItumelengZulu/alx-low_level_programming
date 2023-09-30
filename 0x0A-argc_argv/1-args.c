18257e7d59875b2ac5218a0d069886cfa1961a23
ItumelengZulu <itumelengzulu@gmail.com>
2023-09-30 04:04:48 +0200

    argc & argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * @argc: The number of command-line arguments
+ * @argv: An array of command-line argument strings
+ *
+ * Return: Always 0
+ */
 int main(int argc, char *argv[]) {
     int i;
 
@@ -9,6 +16,6 @@ int main(int argc, char *argv[]) {
         printf("Argument %d: %s\n", i, argv[i]);
     }
 
-    return 0;
+    return (0);
 }
 
