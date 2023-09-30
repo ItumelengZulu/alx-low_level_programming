d6ae7e60cdf8b2e31c6266cc5b4be88f27efda2e
ItumelengZulu <itumelengzulu@gmail.com>
2023-09-30 04:12:25 +0200

    argc & argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,21 +1,15 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - Entry point
  * @argc: The number of command-line arguments
- * @argv: An array of command-line argument strings
  *
  * Return: Always 0
  */
-int main(int argc, char *argv[]) {
-    int i;
-
-    printf("Number of arguments: %d\n", argc - 1);
-
-    for (i = 1; i < argc; i++) {
-        printf("Argument %d: %s\n", i, argv[i]);
-    }
-
-    return (0);
+int main(int argc) {
+    /* Print the number of arguments (excluding the program name) */
+    printf("%d\n", argc - 1);
+    return 0;
 }
 
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -4,5 +4,8 @@
 /* Prototype function */
 int _putchar(char *str);
 
+/* Prototype function */
+/*int main(int argc, char *argv[]);*/
+
 #endif /* MAIN_H */
 
