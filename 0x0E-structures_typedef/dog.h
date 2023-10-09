50fe988d9e37ae4b19addd91a5263b88c6084937
ItumelengZulu <itumelengzulu@gmail.com>
2023-10-09 23:48:10 +0200

    Structures, typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/*
+ * struct dog
+ *
+ * This struct represents a dog. It has three members:
+ *
+ * name: A pointer to a character array containing the dog's name.
+ * age: A floating-point number representing the dog's age in years.
+ * owner: A pointer to a character array containing the dog's owner's name.
+ */
+struct dog {
+  char *name;
+  float age;
+  char *owner;
+};
