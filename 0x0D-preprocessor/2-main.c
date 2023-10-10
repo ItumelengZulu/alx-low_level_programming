c8fdaff9cb11c1a6d896195ba7b4a299bc90ff95
ItumelengZulu <itumelengzulu@gmail.com>
2023-10-10 02:22:12 +0200

    Preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program.
+ *
+ * Return: Always 0 (Success).
+ */
 int main(void)
 {
+    /* Print the name of the source file */
     printf("%s\n", __FILE__);
+
     return 0;
 }
 
