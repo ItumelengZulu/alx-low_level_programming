ec8a7cda581fb6568c755347b2837456de3cfb33
itumelengzulu <itumelengzulu@gmail.com>
2023-10-24 22:18:15 +0200

    More singly linked lists

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -33,5 +33,38 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 /* Function to print elements of a listint_t list */
 listint_t *add_nodeint(listint_t **head, const int n);
 
+/* Function */
+void free_listint(listint_t *head);
+
+/* Function */
+void free_listint2(listint_t **head);
+
+/* Function */
+int pop_listint(listint_t **head);
+
+/* Function */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+
+/* Function */
+int sum_listint(listint_t *head);
+
+/* Function */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+
+/* Function */
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
+
+/* Function */
+listint_t *reverse_listint(listint_t **head);
+
+/* Function */
+size_t print_listint_safe(const listint_t *head);
+
+/* Function */
+size_t free_listint_safe(listint_t **h);
+
+/* Function */
+listint_t *find_listint_loop(listint_t *head);
+
 #endif /* LISTS_H */
 
