92264a3583d5e89813005056bf47e90324f2fb8c
itumelengzulu <itumelengzulu@gmail.com>
2023-10-27 00:55:31 +0200

    Bit manipulation

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Included libraries */
+#include <unistd.h>
+
+/* Function prototypes */
+int _putchar(char c);
+void print_binary(unsigned long int n);
+unsigned int binary_to_uint(const char *b);
+int get_bit(unsigned long int n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
+
+#endif /* MAIN_H */
+
